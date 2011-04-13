00946d5b6b83ebede9aa29af922bdb27bef87712
Andy Whitfield <andy@andywhitfield.me.uk>
2011-04-13 18:32:06 +0100

    Implemented Main method appropriately.

--- StatLightToNUnitReportGenerator/Program.cs
+++ StatLightToNUnitReportGenerator/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 
 namespace StatLightToNUnitReportGenerator
 {
@@ -9,6 +10,11 @@ namespace StatLightToNUnitReportGenerator
     {
         static void Main(string[] args)
         {
+            var inputFile = new StreamReader(args[0]);
+            var outputFile = new StreamWriter(args[1]);
+            var parser = new StatLightReportParser(inputFile);
+            var formatter = new NUnitTestResultsFormatter(outputFile);
+            formatter.Format(parser.Parse());
         }
     }
 }
